f4c7ec312090097dd74fd8ffd2e164c1b8a493ea
Julien Wakim <68880503+JulienWakim@users.noreply.github.com>
2025-02-13 14:46:12 -0500

    Update index.py

--- backend/flask/index.py
+++ backend/flask/index.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS  # Import CORS package
 
 app = Flask(__name__)
+CORS(app)  # Allow all origins (temporary fix)
 
 @app.route('/')
 def home():
@@ -9,3 +11,8 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
