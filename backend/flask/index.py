c1ac07ce6935f1207d3bdffc93ea60200667981b
julienwakim <julienjwakim@gmail.com>
2025-02-17 13:06:33 -0500

    Testing with key

--- backend/flask/index.py
+++ backend/flask/index.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Set up Gemini API
-GEMINI_API_KEY = "TEST"  # Replace with your API key
+GEMINI_API_KEY = "AIzaSyDcUA1UaoAOFXBq_U2aq_2y-yjMSRwVYik"  # Replace with your API key
 genai.configure(api_key=GEMINI_API_KEY)
 
 @app.route("/")
